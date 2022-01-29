59230d241d254c6760b2b7bd42210efb4eac20f6
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2022-01-29 13:35:19 +0530

    chore(web-client): chore: changed base-url to use capitalized api base-url in the client
    capitalized the word api in the base-url when querying via the client
    
    re #11

--- web-client/src/shared/api/client.ts
+++ web-client/src/shared/api/client.ts
@@ -3,53 +3,53 @@ import { AccessTokenPair } from "../interfaces/AccessToken.interfaces";
 import { setAccessToken } from "../redux/slices/auth/authSlice";
 import store from "../redux/store";
 
-const baseURL: string = process.env.REACT_APP_API_URL ?? "http://localhost:4000/api";
+const baseURL: string = process.env.REACT_APP_API_URL ?? "http://localhost:4000/Api";
 
 export const client = axios.create({
-	baseURL,
-	headers: {
-		"Content-Type": "application/json",
-	},
-	withCredentials: true,
-	validateStatus: (status) => {
-		const acceptableStatusCodes = [200, 201, 204, 400];
-		if (acceptableStatusCodes.includes(status)) {
-			return true;
-		}
-		return false;
-	},
+  baseURL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+  withCredentials: true,
+  validateStatus: (status) => {
+    const acceptableStatusCodes = [200, 201, 204, 400];
+    if (acceptableStatusCodes.includes(status)) {
+      return true;
+    }
+    return false;
+  },
 });
 
 client.interceptors.request.use(
-	(config) => {
-		const token = store.getState().auth.access_token;
+  (config) => {
+    const token = store.getState().auth.access_token;
 
-		if (config && config.headers) {
-			config.headers["Authorization"] = "Bearer " + token;
-		}
+    if (config && config.headers) {
+      config.headers["Authorization"] = "Bearer " + token;
+    }
 
-		return config;
-	},
-	async (err) => {
-		const originalRequest = err.config;
+    return config;
+  },
+  async (err) => {
+    const originalRequest = err.config;
 
-		if (err.response.status === 401 && !originalRequest._retry) {
-			originalRequest._retry = true;
-			try {
-				const response = await client.get("/Authentication/Refresh");
-				const data = response.data as AccessTokenPair;
+    if (err.response.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+      try {
+        const response = await client.get("/Authentication/Refresh");
+        const data = response.data as AccessTokenPair;
 
-				if (data.access_token) {
-					store.dispatch(setAccessToken({ accessToken: data.access_token }));
-				}
+        if (data.access_token) {
+          store.dispatch(setAccessToken({ accessToken: data.access_token }));
+        }
 
-				originalRequest.headers.Authorization = `Bearer ${data.access_token}`;
-				return client(originalRequest);
-			} catch (error) {
-				return Promise.reject(error);
-			}
-		}
+        originalRequest.headers.Authorization = `Bearer ${data.access_token}`;
+        return client(originalRequest);
+      } catch (error) {
+        return Promise.reject(error);
+      }
+    }
 
-		return Promise.reject(err);
-	}
+    return Promise.reject(err);
+  }
 );
