996289c4ab48b4331f82084faafb851631c9b5f0
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2022-02-13 00:07:59 +0530

    feat(web-client): feat: reports page searching
    searching of the available reports
    
    re #26

--- web-client/src/modules/ReportsScreen/ReportsScreen.tsx
+++ web-client/src/modules/ReportsScreen/ReportsScreen.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import Typography from "@mui/material/Typography";
 
 import PagePaperWrapper from "../../shared/components/PagePaperWrapper/PagePaperWrapper";
+import SearchForReports from "./SearchForReports";
 
 const ReportsScreen = () => {
   return (
@@ -10,8 +11,38 @@ const ReportsScreen = () => {
       <Typography variant="h4" fontWeight={500} component="h1">
         Reports
       </Typography>
+      <SearchForReports availableReports={REPORTS} />
     </PagePaperWrapper>
   );
 };
+
+const REPORTS = [
+  {
+    id: "report-1",
+    name: "User application usage report",
+  },
+  {
+    id: "report-2",
+    name: "User activity report",
+  },
+  {
+    id: "report-3",
+    name: "Team user activity report",
+  },
+  {
+    id: "report-4",
+    name: "Team live event usage report",
+  },
+  {
+    id: "report-5",
+    name: "Teams PSTN blocked users report",
+  },
+  {
+    id: "report-6",
+    name: "Teams device usage report",
+  },
+];
+
+export type REPORT_LIST_TYPE = typeof REPORTS;
 
 export default ReportsScreen;
---
+++ web-client/src/modules/ReportsScreen/SearchForReports.tsx
@@ -0,0 +1,88 @@
+import React, { useCallback, useMemo, useState } from "react";
+
+import { REPORT_LIST_TYPE } from "./ReportsScreen";
+
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import TextField from "@mui/material/TextField";
+import Autocomplete from "@mui/material/Autocomplete";
+
+import { COMMON_ITEM_BORDER_COLOR } from "../../shared/util/constants";
+
+interface Props {
+  availableReports: REPORT_LIST_TYPE;
+}
+
+const SearchForReports = (props: Props) => {
+  const { availableReports } = props;
+
+  const [selectedReportId, setSelectedReportId] = useState("");
+
+  const formattedReports = useMemo(() => {
+    const reps = availableReports.map((report) => {
+      return { label: report.name, reportId: report.id };
+    });
+    setSelectedReportId(reps[0].reportId);
+    return reps;
+  }, [availableReports]);
+
+  const handleSearchSelectItem = useCallback(
+    (evt: any) => {
+      if (!evt?.target?.value || evt.target.value.length === 0) return;
+
+      const selectedReport = formattedReports.find((report) => report.label === evt.target.value);
+
+      if (selectedReport) setSelectedReportId(selectedReport.reportId);
+    },
+    [formattedReports]
+  );
+
+  return (
+    <Grid container spacing={2}>
+      <Grid item xs={12} md={12}>
+        <Box sx={{ mt: { sm: 0, md: 3 }, pt: { sm: 3, md: 0 } }}>
+          <Autocomplete
+            disablePortal
+            id="combo-box-demo"
+            options={formattedReports}
+            sx={{ width: 300 }}
+            onSelect={handleSearchSelectItem}
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                label="Search reports"
+                size="small"
+                InputProps={{ ...params.InputProps, endAdornment: <></> }}
+              />
+            )}
+          />
+        </Box>
+      </Grid>
+      <Grid item xs={12} md={12}>
+        <Grid container spacing={1}>
+          {formattedReports.map((report) => (
+            <Grid key={`report-item-${report.label}`} item xs={12} md={3}>
+              <Box
+                onClick={() => setSelectedReportId(report.reportId)}
+                sx={{
+                  cursor: "pointer",
+                  px: 2,
+                  py: 1,
+                  bgcolor: selectedReportId === report.reportId ? "primary.50" : "#fff",
+                  borderStyle: "solid",
+                  borderColor: selectedReportId === report.reportId ? "primary.500" : COMMON_ITEM_BORDER_COLOR,
+                  borderWidth: 2,
+                  borderRadius: 3,
+                }}
+              >
+                {report.label}
+              </Box>
+            </Grid>
+          ))}
+        </Grid>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default SearchForReports;
