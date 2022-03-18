afbd0a4be0ecbb1aaf3d2ed7eb190e66172e1030
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2022-03-19 01:56:50 +0530

    fix(web-client): changed vite config

--- web-client-vite/vite.config.ts
+++ web-client-vite/vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()]
-})
+// export default defineConfig({
+//   plugins: [react()],
+//   build: {
+//     sourcemap: true,
+//   },
+// });
+
+export default defineConfig(({ mode }) => ({
+  plugins: mode === "development" ? [react()] : [],
+  esbuild: {
+    jsxInject: `import * as React from 'react'`,
+  },
+}));
